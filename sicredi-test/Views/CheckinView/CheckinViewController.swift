2cff32a1b4b76f8f27b568954ba31ce32c302d37
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2020-11-25 17:45:05 -0300

    Add CheckinView storyboard and segue connection

---
+++ sicredi-test/Views/CheckinView/CheckinViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CheckinViewController.swift
+//  sicredi-test
+//
+//  Created by Vinícius Chagas on 25/11/20.
+//
+
+import UIKit
+
+class CheckinViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var checkinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
